0c294a5fd5e8363753956283f2355c1028d547ad
yoursmengle <yoursmengle@126.com>
2018-02-06 23:13:49 +0800

    LED Ok, add input

--- Makefile
+++ Makefile
@@ -17,7 +17,8 @@ TARGET = IoDrvADC7001
 OTHER_SRC = ./axio/ai.c ./axio/gpio.c ./axio/gsensor.c ./axio/i2c.c ./axio/pwm.c
 OTHER_SRC += ./axio/ao.c ./axio/ADC7001io.c ./axio/pi.c 
  
-INCLUDE = -I./ -I./Components -I./Linux -I./codesys -I/home/jhzhou/sysroot-glibc-linaro-2.21-2017.10-arm-linux-gnueabi/usr/include/
+INCLUDE += -I./ -I./Components -I./Linux -I./codesys -I/home/jhzhou/sysroot-glibc-linaro-2.21-2017.10-arm-linux-gnueabi/usr/include/
+INCLUDE += -I../kernel_am437x/include/
 
 CXX = $(CROSS_COMPILE)gcc
 # TRG_X86 | TRG_PPC | TRG_ARM | TRG_CORTEX
--- axio/gpio.c
+++ axio/gpio.c
@@ -28,34 +28,27 @@ uint32 g_old_DOs[MAX_DO_NUM];
 uint32 g_all_LEDs[MAX_LED_NUM];
 uint32 g_old_LEDs[MAX_LED_NUM];
 
+//   DI  ports 0~17
+uint32 DI_GPIO_PORT[MAX_DI_NUM] = {
+						0*32 + 7,	3*32 + 17,	2*32 + 11,	4*32 + 7, \
+						4*32 + 5, 	4*32 + 3, 	4*32 + 24,	4*32 + 28, \
+						0*32 + 1,	5*32 + 13, 	1*32 + 28,	1*32 + 19, \
+						0*32 + 19,	2*32 + 1,	2*32 + 29,	2*32 + 28, \
+						2*32 + 27, 	2*32 + 26
+					};
+
 //  DO ports 0 ~ 15
 uint32 DO_GPIO_PORT[MAX_DO_NUM] = {	
-						4*32 + 10,\
-						4*32 + 13,\
-						5*32 + 7,\
-						5*32 + 4,\
-						5*32 + 5,\
-						5*32 + 2,\
-						5*32 + 1,\
-						3*32 + 14,\
-						2*32 + 15,\
-						2*32 + 17,\
-						4*32 + 26,\
-						4*32 + 27,\
-						0*32 + 0,\
-						1*32 + 18,\
-						1*32 + 24,\
-						0*32 + 20,\
+						4*32 + 10,	4*32 + 13,	5*32 + 7,	5*32 + 4, \
+						5*32 + 5, 	5*32 + 2, 	5*32 + 1,	3*32 + 14, \
+						2*32 + 15,	2*32 + 17, 	4*32 + 26,	4*32 + 27, \
+						0*32 + 0,	1*32 + 18,	1*32 + 24,	0*32 + 20 \
 					};
 //LED ports  0 ~ 2
-
+// Drive by 2 channels
 uint32 LED_GPIO_PORT[MAX_LED_NUM*2] = {
-						2*32+25,\
-						2*32+24,\
-						5*32+8,\
-						2*32+ 22,\
-						2*32+23,\
-						3*32+1
+						2*32+25,	2*32+24,	5*32+8,\
+						2*32+ 22,	2*32+23,	3*32+1
 						};
 						
 
@@ -76,6 +69,24 @@ uint32 set_output(uint32 port_num)
 	pclose(fp);
 //		printf(cmd);
 	return 0;
+}
+
+uint32 read_input(uint32 port_num)
+{
+	char cmd[80];
+	char val[4]="9999";
+	sprintf(cmd, "cat /sys/class/gpio/gpio%d/value \n", port_num);
+	FILE *fp = popen(cmd, "r");
+	fgets(val,3,fp);
+	printf("*>");
+	printf(val);
+	printf("<*");
+	pclose(fp);
+	if(val[0] == '1') {
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
 uint32 set_input(uint32 port_num)
@@ -113,8 +124,6 @@ uint32 init_LED(void)
 {
 	uint32 i;
 	printf("starting Init the LEDs...\n");
-
-/************************************* for test jhzhou*/
 	for(i = 0; i<MAX_LED_NUM; i++) {
 		g_all_LEDs[i] = 0;
 		g_old_LEDs[i] = 0;
@@ -131,20 +140,41 @@ uint32 init_LED(void)
 	}
 
 	return 0;
-/************************************* for test jhzhou*/
 }
-uint32 init_gpio(void)
+uint32 init_DI(void)
 {
-	printf("starting Init the GPIO...\n");
 
+	
+	printf("starting Init the DIs...\n");
+	for(uint32 i = 0; i< MAX_DI_NUM; i++) {
+		g_all_DIs[i] = 0;
+		uint32 port_no = DI_GPIO_PORT[i];
+		export_port(port_no);
+		set_input(port_no);
+	}
+
+	return 0;
+}
+
+uint32 init_DO(void)
+{
+	printf("starting Init the DOs...\n");
+	for(uint32 i = 0; i< MAX_DO_NUM; i++) {
+		g_all_DOs[i] = 0;
+		g_old_DOs[i] = 0;
+		uint32 port_no = DO_GPIO_PORT[i];
+		export_port(port_no);
+		set_output(port_no);
+		output_low(port_no);
+	}
 	return 0;
 }
 
 uint32 init_DI_DO(void)
 {
 	uint32 i;
-	init_gpio();
-
+	init_DO();
+	init_DI();
 	init_LED();
 
 	return 0;
@@ -152,8 +182,8 @@ uint32 init_DI_DO(void)
 
 uint32 read_DI(uint32 DI_no)
 {
-	
-	return (DI_no%2);
+	uint32 port_no = DI_GPIO_PORT[DI_no];
+	return read_input(port_no);
 }
 
 uint32 read_all_DIs(void)
@@ -169,7 +199,12 @@ uint32 read_all_DIs(void)
 
 uint32 write_DO(uint32 DO_no, uint32 val)
 {
-
+	uint32 port_no = DO_GPIO_PORT[DO_no];
+	if(val) {
+		output_high(port_no);
+	} else {
+		output_low(port_no);
+	}
 	return 0;
 }
 
